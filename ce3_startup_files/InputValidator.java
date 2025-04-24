1694c44e9130488791a127f2a6cce89b46cd731f
schareene <schareeneromero@gmail.com>
2025-04-24 19:53:03 -0400

    Update InputValidator.java

--- ce3_startup_files/InputValidator.java
+++ ce3_startup_files/InputValidator.java
@@ -1,10 +1,11 @@
 /*
  * This is the InputValidator class. Please complete this class with
- * appropiate JavaDoc comments, method and code comments, and the appropiate
- * methods to validate inputs from the user. 
+ * appropriate JavaDoc comments, method and code comments, and the appropriate
+ * Methods to validate inputs from the user. 
  */
 
-public class InputValidator {
+public class InputValidator 
+{
     
 
 }
