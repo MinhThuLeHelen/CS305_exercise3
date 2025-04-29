1105c3ecd65ccfdd689575123f03e53d82bc5920
Jason <jason.moralesalonso23@kzoo.edu>
2025-04-28 21:21:22 -0400

    Updated password testcases

--- ce3_startup_files/InputValidator.java
+++ ce3_startup_files/InputValidator.java
@@ -6,6 +6,48 @@
 
 public class InputValidator 
 {
+    public static boolean valPassword(String password)
+    {
+        if(password.length() > 7)
+        {
+            return checkPass(password);
+            
+        }
     
+        else
+        {
+            System.out.println("Too short");
+            return false;
+        }
+    }
+    public static boolean checkPass(String password)
+    {
+        boolean hasNum = false;
+        boolean hasCap = false;
+        boolean hasLow = false;
+        char c;
+
+        for (int i = 0; i < password.length(); i++)
+        {
+         c = password.charAt(i);
+         if(Character.isDigit(c))
+         {
+            hasNum = true;
+         }
+         else if (Character.isUpperCase(c))
+         {
+            hasCap = true;
+         }   
+         else if (Character.isLowerCase(c))
+        {
+             hasLow = true;
+        }
+        if(hasNum && hasCap && hasLow)
+        {
+            return true;
+        }
+        }
+        return false;
+    }
 
 }
--- ce3_startup_files/InputValidatorTest.java
+++ ce3_startup_files/InputValidatorTest.java
@@ -3,8 +3,11 @@
  * Java applications. For more information check the complete tutorial
  * here: https://www.tutorialspoint.com/junit/index.htm
  */
+import com.sun.source.tree.AssertTree;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
 
 public class InputValidatorTest 
 {
@@ -47,5 +50,39 @@ public class InputValidatorTest
     // Test cases for EMAIL field
 
     // Test cases for USERNAME field
+
+
+
+    @Test
+    public void passwordTest()
+    {
+        assertTrue(InputValidator.valPassword("Password1"));
+    }
+
+    @Test
+    public void shortPasswordTest()
+    {
+        assertFalse(InputValidator.valPassword("Pass1"));
+    }
+
+    @Test
+    public void noNumPasswordTest()
+    {
+        assertFalse(InputValidator.valPassword("myPassword"));
+    }
+    
+    @Test
+    public void lowCasePasswordTest()
+    {
+        assertFalse(InputValidator.valPassword("PASSWORD1"));
+    }
+
+    @Test
+    public void upCasePasswordTest()
+    {
+        assertFalse(InputValidator.valPassword("password1"));
+    }
+    
     
 }
+
